016437cc3efb4ecd5891d2e225ee969a74edf93a
Gamma King <61883930+GammaKing2000@users.noreply.github.com>
2021-04-27 20:47:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from skimage.io import imread
+from skimage.io import imread, imshow
 from skimage.transform import resize
 import streamlit as st
 import numpy as np
@@ -6,7 +6,6 @@ import joblib
 model = joblib.load('img_recog_model')
 st.title('Infinity War Recognizer')
 ip = st.text_input('Enter image URL')
-st.write(ip)
 flat_data = []
 img1 = imread(ip)
 img1_resized = resize(img1,(150,150,3))
@@ -15,6 +14,7 @@ flat_data = np.array(flat_data)
 op = model.predict(flat_data)
 if st.button('Predict'):
   st.title(op[0]) 
+  imshow(ip)
 # numpy==1.19.5
 #joblib==1.0.1
 #scikit_image==0.16.2
