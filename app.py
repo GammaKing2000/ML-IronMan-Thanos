3768bf99aa2ab0e3334c4b2daed60ac1a519305e
Gamma King <61883930+GammaKing2000@users.noreply.github.com>
2021-04-27 21:23:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import joblib
 model = joblib.load('img_recog_model')
 st.title('Infinity War Recognizer')
 st.text('Enter image URL of Ironman or Thanos and the model will predict what it is')
-if st.button('Start'):
-  ip = st.text_input('Enter image URL')
-  flat_data = []
+ip = st.text_input('Enter image URL')
+flat_data = []
+if st.button('Predict'):
   img1 = imread(ip)
   img1_resized = resize(img1,(150,150,3))
   flat_data.append(img1_resized.flatten())
   flat_data = np.array(flat_data)
   op = model.predict(flat_data)
-  if st.button('Predict'):
-    st.title(op[0]) 
-    st.image(img1)
+  st.title(op[0]) 
+  st.image(img1)
